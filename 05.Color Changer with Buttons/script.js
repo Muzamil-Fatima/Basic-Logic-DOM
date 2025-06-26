ec88285eb04f061b4d5ae7a31eb8fe3cb5e6bc12
Muzamil <muzamilzubair987654@gmail.com>
2025-06-26 10:05:52 +0500

    Update file

---
+++ 05.Color Changer with Buttons/script.js
@@ -0,0 +1,37 @@
+const btns = document.querySelectorAll(".btn");
+const body = document.body;
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    number = btn.value;
+    changeBackground(number);
+  });
+});
+
+
+function changeBackground(color){
+    body.className = "";
+
+    switch(color){
+        case "purple":
+            body.classList.add("purple")
+            break;
+        case "red":
+            body.classList.add("red")
+            break;
+        case "teal":
+            body.classList.add("teal")
+            break;
+        case "yellow":
+            body.classList.add("yellow")
+            break;
+        case "green":
+            body.classList.add("green")
+            break;
+        case "blue":
+            body.classList.add("blue")
+            break;
+        default:
+            body.classList.add("teal")
+    }
+}
